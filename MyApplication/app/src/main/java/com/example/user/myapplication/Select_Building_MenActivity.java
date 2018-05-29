ea68582afca01f21d74cfacc617f04b02d1b961a
JeonEunpyo <thdflaakxm1@naver.com>
2018-05-30 01:31:52 +0900

    no message

--- MyApplication/app/src/main/java/com/example/user/myapplication/Select_Building_MenActivity.java
+++ MyApplication/app/src/main/java/com/example/user/myapplication/Select_Building_MenActivity.java
@@ -1,12 +1,27 @@
 package com.example.user.myapplication;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.ImageButton;
 
 public class Select_Building_MenActivity extends AppCompatActivity {
+    ImageButton engineering_button_men;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_select_building_men);
+
+        engineering_button_men = (ImageButton) findViewById(R.id.building3_Button_m);
+
+        engineering_button_men.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intentLoadNewActivity = new Intent(Select_Building_MenActivity.this, Engineering_MenActivity.class);
+                startActivity(intentLoadNewActivity);
+            }
+        });
     }
 }
